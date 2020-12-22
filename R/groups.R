13e6729f0c118e90267283cd8e778e1674714d20
Heidrun Heidarsdottir <heidrunh17@ru.is>
2020-12-22 02:29:47 +0000

    Added check for number of groups

--- R/groups.R
+++ R/groups.R
@@ -14,34 +14,41 @@ cutListByElement <- function(fullList, element) {
 # First variable is total number of objects to divide into groups (the population)
 # Second variable is an array of group sizes
 randAssign <- function(totalPopulation, ...) {
-  groupSizes <- c()
-  listOfGroups <- c()
+  if (length(list(...)) == 0) {
+    return("Number of group sizes must be more than 0!")
+  }
   if (totalPopulation <= 0) { # Check if totalPopulation is bigger than 0
     return("Total population must be greater than 0!")
   }
+
+  groupSizes <- c()
+  listOfGroups <- c()
+
   for (i in list(...)) {
     groupSizes <- c(groupSizes, i)
   }
+
   if ((totalPopulation %% min(groupSizes)) != 0) { # Check if totalPopulation is divisible by smallest group size
     return("Total population must be divisible by the smallest group number")
   }
-    groupSizes <- unique(groupSizes) # Only get one instance of each group size from array
-    groupSizes <- sort(groupSizes, decreasing=TRUE) # Sort the array to get the largest size first
 
-    while(totalPopulation > 0) {
-      newGroup <- randElem(groupSizes) # Get a random group size
+  groupSizes <- unique(groupSizes) # Only get one instance of each group size from array
+  groupSizes <- sort(groupSizes, decreasing=TRUE) # Sort the array to get the largest size first
 
-      if (newGroup > totalPopulation) { # If the given new group is too large, cut all groups larger than it from the list of group sizes
-        while(newGroup > totalPopulation) { # Loop and cut until too large elements have been cut away
-          groupSizes <- cutListByElement(groupSizes, newGroup)
-          newGroup <- randElem(groupSizes)
-        }
+  while(totalPopulation > 0) {
+    newGroup <- randElem(groupSizes) # Get a random group size
+
+    if (newGroup > totalPopulation) { # If the given new group is too large, cut all groups larger than it from the list of group sizes
+      while(newGroup > totalPopulation) { # Loop and cut until too large elements have been cut away
+        groupSizes <- cutListByElement(groupSizes, newGroup)
+        newGroup <- randElem(groupSizes)
       }
-
-      totalPopulation <- totalPopulation - newGroup
-      listOfGroups <- append(listOfGroups, newGroup)
-
     }
+
+    totalPopulation <- totalPopulation - newGroup
+    listOfGroups <- append(listOfGroups, newGroup)
+
+  }
 
   listOfGroups
 }
---
+++ README.md
@@ -0,0 +1,22 @@
+# Groups Package
+
+This is an R pacakge that contains functions for operations such as to divide a population into different group sizes. It also contains helper functions that can be used for different purposes.
+
+# Usage
+Install the package:
+`devtools::install_github('davidissamattos/ESEData')`
+
+Load the package:
+`library(ESEData)`
+
+Use the functions:
+`randomGroups <- randAssign(144, 12, 20, 4, 12)
+randomGroups`
+
+Example output:
+`12 20  4 20  4  4  4  4 12 12 12 20  4  4  4  4`
+
+List of functions:
+1. randAssign(totalPopulation, groupSize1, ...) - Randomly assign groups to a given population
+2. cutListByElement(list, element) - Cut all elements from a list, up to the given element and return the rest
+3. randElem(list) - Get a list and choose a random element from it
